7391454c66f75f9891c6e83b936a84bc411de410
fjc59 <fjc59@drexel.edu>
2024-04-20 16:08:49 -0400

    Send data from backend to frontend works

--- front/src/Components/Example.jsx
+++ front/src/Components/Example.jsx
@@ -1,3 +1,15 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
 export default function Example() {
+  let fetchExample = async () => {
+    let response = await axios.get("/api/example");
+    console.log(response);
+  };
+
+  useEffect(() => {
+    fetchExample();
+  }, []);
+
   return <></>;
 }
