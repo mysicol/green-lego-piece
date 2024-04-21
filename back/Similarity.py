69691c21396e7b71d648792d9bce44c1b7487613
Abby <117459906+mysicol@users.noreply.github.com>
2024-04-20 21:14:39 -0400

    Turning similarity into a number

--- back/Similarity.py
+++ back/Similarity.py
@@ -10,13 +10,12 @@ class Similarity:
         
         APIKeys.set_var(VAR_NAME)
         self.__apikey = APIKeys.get_key(VAR_NAME)
-        print(self.__apikey)
 
     def getRating(self, article):
         body = { 'text_1': self.__query, 'text_2': article }
         response = requests.post(self.__api_url, headers={'X-Api-Key': self.__apikey}, json=body)
         
         if response.status_code == requests.codes.ok:
-            return response.text
+            return float(response.text.split(" ")[1][:-1])
         else:
             print("Error:", response.status_code, response.text)
